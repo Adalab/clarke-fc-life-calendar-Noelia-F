96f6723e2e2cd1014cfb1966a564b1da90931c7e
Noelia-F <noelia.fronton.g@gmail.com>
2018-03-21 22:51:53 +0100

    header width style

--- my-life-calendar/src/components/header/Header.js
+++ my-life-calendar/src/components/header/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './header.css';
+import './plus2.svg';
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
-      
+        <div className="header__plus">
+        </div>
       </header>
     );
   }
