959ef1367bc16f80100487d9a6da73d89c5b58c0
Noelia-F <noelia.fronton.g@gmail.com>
2018-03-22 12:43:02 +0100

    apply router

--- my-life-calendar/src/app/App.js
+++ my-life-calendar/src/app/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import Header from '../components/header/Header.js';
 import Calendar from '../components/calendar/Calendar.js';
 import Editor from '../components/editor/Editor.js';
+import {Link, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Link className="link" to={`/editor`}>
+          <Header/>
+        </Link>
         <Calendar/>
-        <Editor/>
+        <Switch>
+          <Route exact path={`/editor`} render = {() =>
+            <Editor/> } />
+        </Switch>
       </div>
     );
   }
--- my-life-calendar/src/components/editorButtons/EditorButtons.js
+++ my-life-calendar/src/components/editorButtons/EditorButtons.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './editorButtons.css';
+import App from '../../app/App';
+import {Link, Route, Switch} from 'react-router-dom';
 
 class EditorButtons extends Component {
   render() {
     return (
       <div className="editor__buttons">
-        <input className="button" type="submit" value="GUARDAR"></input>
-        <input className="button button--cancel" type="button" value="CANCELAR"></input>
+        <Link className="link" to='/'>
+          <input className="button" type="submit" value="GUARDAR"></input>
+        </Link>
+        <Link className="link" to='/'>
+          <input className="button button--cancel" type="button" value="CANCELAR"></input>
+        </Link>
+        <Switch>
+          <Route exact path="/" component={App} />
+        </Switch>
       </div>
     );
   }
