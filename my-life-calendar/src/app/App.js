a3ca59f04b4e3a4882a720a37b64cf6c17c6026d
Noelia-F <noelia.fronton.g@gmail.com>
2018-03-22 19:29:03 +0100

    preparing to funcionality

--- my-life-calendar/src/app/App.js
+++ my-life-calendar/src/app/App.js
@@ -6,6 +6,12 @@ import Editor from '../components/editor/Editor.js';
 import {Link, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
   render() {
     return (
       <div className="App">
